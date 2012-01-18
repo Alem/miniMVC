17578349344102e3491a2bd1e1c85f6429b5f1a4
za <info@alemmedia.com>
2012-01-18 14:01:37 -0700

    Nice Model

--- lib/model.php
+++ lib/model.php
@@ -18,14 +18,17 @@ class Model{
 	}
 
 	public function query( $query, $row = 1 ){
-		$this->db_connect();
-		$result = mysql_query($query);
-		while( $query = mysql_fetch_assoc($result) or die('No match found') ) {
-			$query_array[$row] = $query;
-			$row++;
+		$this -> db_connect();
+		$result = mysql_query($query) or die('Query failed :(');
+		if ( preg_match('/select/', $query) ){
+			while( $query = mysql_fetch_assoc($result) ) {
+				$query_array[$row] = $query;
+				$row++;
+			}
+			$this -> db_disconnect();
+			return $query_array;
 		}
-		$this->db_disconnect();
-		return $query_array;
+		$this -> db_disconnect();
 	}
 
 }
