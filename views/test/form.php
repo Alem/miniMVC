e2c7370f3d504fd6c57d30f72b0a522bc208c8f3
za <info@alemmedia.com>
2012-01-19 22:58:58 -0700

    Added a view: form

---
+++ views/test/form.php
@@ -0,0 +1,5 @@
+<h2>Add an Item</h2>
+<form action = "?test/form/" method="post">
+<label>Item: </label> <input id = "item" name = "item" type="text" />
+<input type = "submit" value = "Add"/>
+</form>
